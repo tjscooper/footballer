6d9707daf905fa12e9c89c35ed2a32d8ef655f5b
Tim Cooper <tcooper@weedmaps.com>
2016-09-01 08:00:29 -0400

    Add private tasks and publications

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -45,9 +45,19 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   render() {
@@ -95,6 +105,8 @@ App.propTypes = {
 };
 
 export default createContainer(() => {
+
+  Meteor.subscribe('tasks');
 
   let tasks = Tasks.find({}, { sort: { createdAt: -1 } }).fetch();
   let incompleteCount = Tasks.find({ checked: { $ne: true } }).count();
